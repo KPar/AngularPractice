be75ed0ae386f60e6d525b3d1bf4fdbd317d4d96
KPar <kpar1991@gmail.com>
2016-07-25 14:30:17 +1200

    Added function to "Add" Button

--- scripts/script.js
+++ scripts/script.js
@@ -25,4 +25,8 @@ todoApp.controller("ToDoCtrl", function ($scope) {
     $scope.warningLevel = function () {
         return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
     }
+
+    $scope.addNewItem = function (actionText) {
+        $scope.todo.items.push({action: actionText, done: false})
+    }
 });
