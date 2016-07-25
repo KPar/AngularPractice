4058aa997a19247356743c8a92fbf17b601edd1a
KPar <kpar1991@gmail.com>
2016-07-25 14:13:30 +1200

    added warning label

--- scripts/script.js
+++ scripts/script.js
@@ -13,4 +13,16 @@ var todoApp = angular.module("todoApp",[]);
 
 todoApp.controller("ToDoCtrl", function ($scope) {
     $scope.todo = model;
+    
+    $scope.incompleteCount = function () {
+        var count = 0;
+        angular.forEach($scope.todo.items, function (item) {
+            if (!item.done) {count++}
+        });
+        return count;
+    }
+
+    $scope.warningLevel = function () {
+        return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+    }
 });
